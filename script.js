460c6f17230906963c14aba900f19af4f1d9ac02
Daniel Burgess <37884090+Danb981@users.noreply.github.com>
2022-11-04 18:24:39 -0400

    Basic functionality added

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+const screen = document.querySelector(".screen");
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => button.addEventListener("click", buttonClicked));
+
+let bufferNum;
+let operator;
+let screenNum = "";
+
 function add(x, y){
-    return x + y;
+    return +x + +y;
 }
 function subtract(x, y){
     return x - y;
@@ -10,6 +18,32 @@ function multiply(x, y){
 function divide(x, y){
     return x / y;
 }
-function operate(){
-    
+function operate(x, y, operator){
+    if(operator === '+'){
+        screenNum = add(x, y);
+    }
+    else if(operator === '-'){
+        screenNum = subtract(x, y);
+    }
+    else if(operator === 'x'){
+        screenNum = multiply(x, y);
+    }
+    else if(operator === '÷'){
+        screenNum = divide(x, y);
+    }
+}
+function buttonClicked(e){
+    let buttonText = e.target.innerText;
+    if(!isNaN(buttonText) || buttonText === '.'){
+        screenNum += buttonText;
+    }
+    else if(buttonText === '='){
+        operate(bufferNum, screenNum, operator);
+    }
+    else{
+        bufferNum = screenNum;
+        screenNum = "";
+        operator = buttonText;
+    }
+    screen.value = screenNum;
 }
