29f5de7c42cc5907cd082ad993ca8959cf5e46c2
Daniel Burgess <37884090+Danb981@users.noreply.github.com>
2022-11-04 22:22:44 -0400

    Added logic to perform operation when hitting an operator instead of equals

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ const screen = document.querySelector(".screen");
 const buttons = document.querySelectorAll("button");
 buttons.forEach((button) => button.addEventListener("click", buttonClicked));
 
-let bufferNum;
-let operator;
+let bufferNum = "";
+let operator = "";
 let screenNum = "";
+let overwrite = false;
 
 function add(x, y){
     return +x + +y;
@@ -20,30 +21,56 @@ function divide(x, y){
 }
 function operate(x, y, operator){
     if(operator === '+'){
-        screenNum = add(x, y);
+        screenNum = add(x, y).toString();
     }
     else if(operator === '-'){
-        screenNum = subtract(x, y);
+        screenNum = subtract(x, y).toString();
     }
     else if(operator === 'x'){
-        screenNum = multiply(x, y);
+        screenNum = multiply(x, y).toString();
     }
     else if(operator === '÷'){
-        screenNum = divide(x, y);
+        screenNum = divide(x, y).toString();
     }
+    bufferNum = "";
+    operator = "";
 }
 function buttonClicked(e){
     let buttonText = e.target.innerText;
     if(!isNaN(buttonText) || buttonText === '.'){
+        if(overwrite){
+            overwrite = !overwrite;
+            screenNum = "";
+            screen.value = screenNum;
+        }
         screenNum += buttonText;
     }
     else if(buttonText === '='){
         operate(bufferNum, screenNum, operator);
     }
-    else{
-        bufferNum = screenNum;
+    else if(buttonText === 'Clear'){
+        bufferNum = "";
+        operator = "";
         screenNum = "";
-        operator = buttonText;
+    }
+    else if(buttonText === 'Delete'){
+        screenNum = screenNum.slice(0, screenNum.length - 1);
+    }
+    else{ //operation click
+        if(bufferNum != ""){
+            operate(bufferNum, screenNum, operator);
+            operator = buttonText;
+            bufferNum = screenNum;
+            overwrite = true;
+        }
+        else{
+            bufferNum = screenNum;
+            screenNum = "";
+            operator = buttonText;
+        }
     }
     screen.value = screenNum;
+    console.log({bufferNum});
+    console.log({operator});
+    console.log({screenNum});
 }
